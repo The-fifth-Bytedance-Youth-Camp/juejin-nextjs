1b07e74b4e9e78d809a74d231e8f1e6e0acded93
自然醒 <2507983704@qq.com>
2023-01-24 16:04:57 +0800

    feat(project): add the website theme toggle function

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,10 @@
 import { ConfigProvider } from 'antd';
 import { useEffect } from 'react';
 import { rem } from '@/assets/js/rem';
+import '@/assets/styles/init.css';
+import '@/assets/styles/markdown/github-markdown.scss';
+import '@/assets/styles/theme/light.scss';
+import '@/assets/styles/theme/dark.scss';
 
 const theme = {
 	token: {

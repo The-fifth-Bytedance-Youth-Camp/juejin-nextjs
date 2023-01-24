295a88e62a5a5215e43c52bf865c8646a97dbd21
自然醒 <2507983704@qq.com>
2023-01-24 16:10:49 +0800

    feat(post): add a back top button for the page

---
+++ src/utils/hooks/useScrolling.js
@@ -0,0 +1,27 @@
+import { useState, useEffect, useRef } from 'react';
+
+function useIsScrolling() {
+	const [ isScrolling, setIsScrolling ] = useState(false);
+	const timeoutIdRef = useRef(null);
+
+	useEffect(() => {
+		const handleScroll = () => {
+			clearTimeout(timeoutIdRef.current);
+			setIsScrolling(true);
+			timeoutIdRef.current = setTimeout(() => {
+				setIsScrolling(false);
+			}, 800);
+		};
+
+		window.addEventListener('scroll', handleScroll);
+
+		return () => {
+			clearTimeout(timeoutIdRef.current);
+			window.removeEventListener('scroll', handleScroll);
+		};
+	}, []);
+
+	return isScrolling;
+}
+
+export default useIsScrolling;

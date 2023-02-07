334cedc447a2755c774eac8b5467522af3ce10ee
张承桢 <10616796+zcz666@user.noreply.gitee.com>
2023-02-07 20:09:26 +0800

    dark

--- src/components/post/sidebar/index.jsx
+++ src/components/post/sidebar/index.jsx
@@ -14,7 +14,7 @@ import MarkNav from "markdown-navbar";
 const Sidebar = ({ content }) => {
 	return (
 		<div className={ style.Sidebar } >
-		<Card title="相关文章" style={{ width: 300 }} >
+		<Card className={ style.artical} title="相关文章" style={{ width: 300 }} bordered={false}>
 			<p className={ style.related }>共克时疫 | 面对疫情，掘金开发者们贡献出的那些技术力量</p>
 			<div className={ style.extra }>
 			<div className={ style.like }>77点赞  · </div>
@@ -26,10 +26,10 @@ const Sidebar = ({ content }) => {
 			<div className={ style.comment }>85评论</div>
 			</div>
 		</Card>
-		<Card className={ style.next } title="下一篇" style={{ width: 300 }}>
+		<Card className={ style.next } title="下一篇" style={{ width: 300 }} bordered={false}>
 			<p>🏆 技术专题第一期 | 聊聊 Deno的一些事儿</p>
 		</Card>
-		<Card  className={ style.menuNav } title="目录" style={{ width: 300 }}>
+		<Card  className={ style.menuNav } title="目录" style={{ width: 300 }} bordered={false}>
 			<MarkNav source={ content } />
 		</Card>
 		</div>
